7e161dd77f2fcd9cdb1aa0d40625adf54e78d13a
KatyaM8 <113946265+KatyaM8@users.noreply.github.com>
2022-10-18 11:36:56 +1000

    Add files via upload

---
+++ 915.a.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+int main() {
+	int n = 0;
+	int k = 0;
+	std::cin >> n >> k;
+	std::vector<int> a(n);
+	for (int i = 0; i < n; i++) {
+		std::cin >> a[i];
+	}
+	// Сортировка массива пузырьком
+	int temp = 0;
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = 0; j < n - i - 1; j++) {
+			if (a[j] > a[j + 1]) {
+				// меняем элементы местами
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+	}
+	for (int i = n - 1; i >= 0; i = i - 1) {
+		if (k % a[i] == 0) {
+			std::cout << k / a[i];
+			break;
+		}
+	}
+}
