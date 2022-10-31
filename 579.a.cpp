3913fc10180d6c57dea0d5ce198daeae32fcdc77
KatyaM8 <113946265+KatyaM8@users.noreply.github.com>
2022-11-01 07:04:11 +1000

    Add files via upload

--- 579.a.cpp
+++ 579.a.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
 int main() {
-    int n =0;
-    std::cin>>n;
-    int k1 = 0;
-    while (n > 0) {
-        if (1 == n % 2) {
-            k1 += 1;
-        }
-        n = n / 2;
-    }
-    std::cout<<k1;
+	int n = 0;
+	std::cin >> n;
+	int k = 536870912;
+	int kolvo_1 = 0;
+	while (k > 0) {
+		if ((k & n) == k) {
+			kolvo_1 = kolvo_1 + 1;
+		}
+		k = k >> 1;
+	}
+	std::cout << kolvo_1;;
 }
