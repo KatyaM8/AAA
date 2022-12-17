354a2313f9aaf4a0a7b27b4e9ce02cda9700b7e9
KatyaM8 <113946265+KatyaM8@users.noreply.github.com>
2022-12-17 15:09:39 +0300

    Add files via upload

---
+++ 1717.b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void solve()
+{
+	int n, k, x, y, fixer;
+	std::cin >> n >> k >> x >> y;
+	fixer = (k + (y - 1) % k - (x - 1) % k);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if ((i + fixer) % k == j % k) {
+				std::cout << "X";
+			}
+			else {
+				std::cout << ".";
+			}
+		}
+		std::cout << std::endl;
+	}
+}
+
+int main() {
+	int t;
+	std::cin >> t;
+	for (int i = 0; i < t; i++) solve();
+}
